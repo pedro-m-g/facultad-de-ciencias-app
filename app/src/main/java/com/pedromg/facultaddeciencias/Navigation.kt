4055c982e74f87d246526d5ebca06f1775319184
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2023-10-04 19:17:50 -0700

    Actualizar componente Navigation. Agregar NavigationDrawer.

--- app/src/main/java/com/pedromg/facultaddeciencias/Navigation.kt
+++ app/src/main/java/com/pedromg/facultaddeciencias/Navigation.kt
@@ -1,17 +1,147 @@
 package com.pedromg.facultaddeciencias
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pedromg.facultaddeciencias.ui.views.NewsView
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.NewsScreen.route) {
-        composable(route = Screen.NewsScreen.route) {
-            NewsView()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    var selectedRoute by rememberSaveable {
+        mutableStateOf(Screen.NewsScreen.route)
+    }
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(
+                    verticalArrangement = Arrangement.Bottom,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .background(Color(0xFF2E7D32))
+                        .padding(16.dp)
+                ){
+                    Text(
+                        text = "Logo",
+                        color = Color.White
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "Facultad de Ciencias",
+                    fontSize = 28.sp,
+                    modifier = Modifier
+                        .padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+
+                NavigationDrawerItem(
+                    label = {
+                        Text(text = "Noticias")
+                    },
+                    selected = selectedRoute == Screen.NewsScreen.route,
+                    onClick = {
+                        selectedRoute = Screen.NewsScreen.route
+                        navController.navigate(selectedRoute)
+                        scope.launch {
+                            drawerState.close()
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Home,
+                            contentDescription = "Noticias"
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+            }
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "Facultad de Ciencias UABC")
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                scope.launch {
+                                    drawerState.open()
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Menú"
+                            )
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            Surface(
+                modifier = Modifier
+                    .padding(innerPadding)
+            ) {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.NewsScreen.route
+                ) {
+                    composable(
+                        route = Screen.NewsScreen.route
+                    ) {
+                        NewsView()
+                    }
+                }
+            }
         }
     }
 }
